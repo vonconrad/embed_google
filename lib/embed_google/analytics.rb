cbc9409730e1dc96726e3b59adab547915d308bd
vonconrad <sebastian@e-channel.com.au>
2011-07-16 20:03:50 +0930

    Add simplest version of analytics code

--- lib/embed_google/analytics.rb
+++ lib/embed_google/analytics.rb
@@ -10,5 +10,17 @@ module EmbedGoogle
       self.account_id = id || self.class.id
       raise ArgumentError, 'Account ID is required' unless account_id
     end
+
+    def script
+      str  = "var _gaq = _gaq || [];\n"
+      str += "_gaq.push(['_setAccount', '#{account_id}']);\n"
+      str += "_gaq.push(['_trackPageview']);\n\n"
+      str += "(function() {\n"
+      str += "\tvar ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n"
+      str += "\tga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n"
+      str += "\tvar s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n"
+      str += "})();\n"
+      "<script type='text/javascript'>\n" + str + "</script>\n"
+    end
   end
 end
--- spec/embed_google/analytics.rb
+++ spec/embed_google/analytics.rb
@@ -18,4 +18,21 @@ describe EmbedGoogle::Analytics do
     instance = EmbedGoogle::Analytics.new('UA-1234567-2')
     instance.account_id.should == 'UA-1234567-2'
   end
+
+  it 'returns the simplest variation of Analytics javascript' do
+    javascript = EmbedGoogle::Analytics.new('UA-1234567-1').script
+    javascript.should == <<-SCRIPT
+<script type='text/javascript'>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1234567-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+SCRIPT
+  end
 end
