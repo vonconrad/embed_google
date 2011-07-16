d8a13f8f82effae8fbb412156c85caf7b7220415
vonconrad <sebastian@e-channel.com.au>
2011-07-16 20:14:57 +0930

    Use two spaces instead of tabs

--- lib/embed_google/analytics.rb
+++ lib/embed_google/analytics.rb
@@ -16,9 +16,9 @@ module EmbedGoogle
       str += "_gaq.push(['_setAccount', '#{account_id}']);\n"
       str += "_gaq.push(['_trackPageview']);\n\n"
       str += "(function() {\n"
-      str += "\tvar ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n"
-      str += "\tga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n"
-      str += "\tvar s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n"
+      str += "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n"
+      str += "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n"
+      str += "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n"
       str += "})();\n"
       "<script type='text/javascript'>\n" + str + "</script>\n"
     end
